70fd21304b647a98fa0a25f78fe6f307e7d23f38
Kireh <Kireh@Kireh>
2017-03-08 16:56:33 -0500

    hello

--- src/Function.java
+++ src/Function.java
@@ -1,4 +1,5 @@
-public class Function{
+public class Function
+{
 	 public static void main(String[] args){
 		System.out.println("hello world");
 	}
--- src/Graphable.java
+++ src/Graphable.java
@@ -1,5 +1,6 @@
 public class Graphable
 {
+	
 	public static void main(String[] args)
 	{
 		System.out.println("hello");
