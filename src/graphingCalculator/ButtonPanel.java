d56cf7a80a3d7b3814dfce13a1c5ed6a535b1b14
kireh <kirehw@gmail.com>
2017-03-11 18:10:49 -0500

    Added ButtonPanel and TablePanel classes

---
+++ src/graphingCalculator/ButtonPanel.java
@@ -0,0 +1,20 @@
+package graphingCalculator;
+
+import javax.swing.JPanel;
+
+/**
+ * This class will hold and control data for the bottom button panel,
+ * which contains graph related functions.
+ * @author kireh
+ *
+ */
+public class ButtonPanel extends JPanel
+{
+	/**
+	 * Creates a new ButtonPanel containing all functions specified in the planning document
+	 */
+	public ButtonPanel()
+	{
+		
+	}
+}
---
+++ src/graphingCalculator/TablePanel.java
@@ -0,0 +1,20 @@
+package graphingCalculator;
+
+import javax.swing.JPanel;
+
+/**
+ * This class will hold and control the TablePanel, which is responsible for 
+ * scrolling along with the GraphPanel and displaying the proper value sets
+ * @author kireh
+ *
+ */
+public class TablePanel extends JPanel
+{
+	/**
+	 *Creates a new TablePanel object with  default TableSet parameters
+	 */
+	public TablePanel()
+	{
+		
+	}
+}
